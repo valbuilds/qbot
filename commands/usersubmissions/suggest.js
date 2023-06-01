e6134cfdf34a1655146cd2ecc5223c0b9a8d658c
Katie <katie@katiebuilds.website>
2023-05-31 23:12:40 -0500

    aaa

--- commands/usersubmissions/suggest.js
+++ commands/usersubmissions/suggest.js
@@ -1,7 +1,203 @@
-const { SlashCommandBuilder, EmbedBuilder } = require(`discord.js`);
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require(`discord.js`);
+const { inlineCode } = require(`discord.js`);
+
+const adminRoleId = `1111908821422587924`;
+const logChannelId = `1111908875172597820`;
+const suggestionChannelId = `1111908862057005127`;
+
+const tagToBeClaimed = `1113562445403914281`;
+const tagAccepted = `1113562292454437074`;
+const tagInProgress = `1113562257763356692`;
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName(`suggest`)
-        .setDescription(`Suggest a server feature.`)
+        .setName(`suggest`).setDescription(`Suggest a server feature.`)
+        .addSubcommand(subcommand => subcommand.setName(`submit`).setDescription(`Submits a suggestion.`)
+            .addStringOption(option => option.setName(`title`).setDescription(`Suggestion title. Max 100 charachters, it's a Discord limitation.`).setMaxLength(100).setRequired(true))
+            .addStringOption(option => option.setName(`description`).setDescription(`Explain in more detail. Max 4096 charachters, it's a Discord limiiation.`).setMaxLength(4096).setRequired(false)))
+        .addSubcommand(subcommand => subcommand.setName(`claim`).setDescription(`(ADMIN+) Claims a suggestion.`))
+        .addSubcommand(subcommand => subcommand.setName(`delete`).setDescription(`(ADMIN+) Deletes a suggestion.`)
+            .addStringOption(option => option.setName(`reason`).setDescription(`Why did you deny this suggestion? Max 2000 charachters, it's a Discord limitation.`).setMaxLength(2000).setRequired(true)))
+        .addSubcommand(subcommand => subcommand.setName(`finalize`).setDescription(`(ADMIN+) Finalizes a suggestion.`)
+            .addStringOption(option => option.setName(`notes`).setDescription(`Additional notes. Max 2000 charachters, it's a Discord limitation.`).setMaxLength(2000).setRequired(true))),
+    async execute (interaction) {
+        const sub = interaction.options.getSubcommand();
+        const member = interaction.member;
+
+        const adminRole = interaction.guild.roles.cache.get(`${adminRoleId}`);
+        const logChannel = interaction.client.channels.cache.get(`${logChannelId}`);
+        const suggestionChannel = interaction.client.channels.cache.get(`${suggestionChannelId}`);
+
+        const avatar = interaction.member.user.avatarURL(
+            { format: 'png', size: 512, forceStatic: false }
+        );
+
+        if (sub === `submit`) {
+            submitSug();
+        } else if (sub === `claim`) {
+            claimSug();
+        } else if (sub === `delete`) {
+            denySug();
+        } else if (sub === `finalize`) {
+            finalizeSug();
+        } else {
+            return interaction.reply({ content: `I'm sorry but you need to run ${inlineCode(`/interaction submit`)}, ${inlineCode(`/interaction claim`)}, ${inlineCode(`/interaction deny`)}, ${inlineCode(`/interaction finalize`)}.`, ephemeral: true });
+        }
+
+        async function submitSug() {
+            const title = interaction.options.getString(`title`);
+            const description = interaction.options.getString(`description`);
+            const tag = Math.floor(Math.random() * (9999 - 1000) + 1000);
+
+            const suggested = new EmbedBuilder()
+                .setTitle(`${title}`)
+                .setDescription(`${description}`)
+                .setAuthor({ name: `${member.displayName}`, iconURL: `${avatar}` })
+                .setFooter({ text: `This message includes member-submitted content. Queer Bot nor it's contributors condone the content. ▪ Suggestion ${tag}` })
+                .setColor(`Blurple`)
+            
+            const created = await suggestionChannel.threads.create({ name: `${title}`, message: { embeds: [suggested] }, appliedTags: [`${tagToBeClaimed}`] });
+            return interaction.reply({ content: `Suggestion sent!\n${created.url}`, ephemeral: true });
+        }
+        async function claimSug() {
+            if (interaction.member.roles.cache.some(role => role.id === `${adminRoleId}`)) {
+                checkForForum();
+            } else {
+                noPerms();
+            }
+
+            async function checkForForum() {
+                if (interaction.channel.isThread) {
+                    isThread();
+                } else {
+                    notThread();
+                }
+            }
+
+            async function isThread() {
+                if (interaction.channel.isLocked) {
+                    alreadyDone();
+                } else {
+                    claimFinale();
+                }
+            }
+
+            async function claimFinale() {
+                const claimed = new EmbedBuilder()
+                    .setTitle(`Suggestion claimed!`)
+                    .setDescription(`<@${interaction.member.id}> will handle your suggestion.`)
+                    .setColor(`Green`)
+
+                await interaction.channel.setAppliedTags([`${tagInProgress}`]);
+                return interaction.reply({ embeds: [claimed] });
+            }
+
+            async function noPerms() {
+                return interaction.reply({ content: `You are not allowed to use this command.`, ephemeral: true });
+            }
+            async function notThread() {
+                return interaction.reply({ content: `You are not allowed to use this command in this channel.`, ephemeral: true });
+            }
+            async function alreadyDone() {
+                return interaction.reply({ content: `This suggestion is already completed.`, ephemeral: true });
+            }
+        }
+        async function denySug() {
+            if (interaction.member.roles.cache.some(role => role.id === `${adminRoleId}`)) {
+                checkForThread();
+            } else {
+                noPerms();
+            }
+
+            async function checkForThread() {
+                if (interaction.channel.isThread) {
+                    checkLocked();
+                } else {
+                    notThread();
+                }
+            }
+
+            async function checkLocked() {
+                if (interaction.channel.isLocked) {
+                    isLocked();
+                } else {
+                    notLocked();
+                }
+            }
+
+            async function notLocked() {
+                const reason = interaction.options.getString(`reason`);
+
+                const deleted = new EmbedBuilder()
+                    .setTitle(`Suggestion deleted`)
+                    .setDescription(`${reason}`)
+                    .setAuthor({ name: `${member.displayName}`, iconURL: `${avatar}` })
+                    .setFooter({ text: `This message includes member-submitted content. Queer Bot nor it's contributors condone the content.` })
+                    .addFields({ name: `Suggestion title:`, value: `${interaction.channel.name}` })
+                    .setColor(`Red`)
+
+                await logChannel.send({ embeds: [deleted] });
+                return interaction.channel.delete(`${reason}`);
+            }
+
+            async function noPerms() {
+                return interaction.reply({ content: `You are not allowed to use this command.`, ephemeral: true });
+            }
+            async function notThread() {
+                return interaction.reply({ content: `You are not allowed to use this command in this channel.`, ephemeral: true });
+            }
+            async function isLocked() {
+                return interaction.reply({ content: `This suggestion is already completed.`, ephemeral: true });
+            }
+        }
+        async function finalizeSug() {
+            if (interaction.member.roles.cache.some(role => role.id === `${adminRoleId}`)) {
+                checkForThread();
+            } else {
+                noPerms();
+            }
+
+            async function checkForThread() {
+                if (interaction.channel.isThread) {
+                    checkLocked();
+                } else {
+                    notThread();
+                }
+            }
+
+            async function checkLocked() {
+                if (interaction.channel.isLocked) {
+                    isLocked();
+                } else {
+                    notLocked();
+                }
+            }
+
+            async function notLocked() {
+                const notes = interaction.options.getString(`notes`);
+
+                const completed = new EmbedBuilder()
+                    .setTitle(`Suggestion completed!`)
+                    .setDescription(`${notes}`)
+                    .setAuthor({ name: `${member.displayName}`, iconURL: `${avatar}` })
+                    .setFooter({ text: `This message includes member-submitted content. Queer Bot nor it's contributors condone the content.` })
+                    .setColor(`Green`)
+
+                await interaction.channel.setAppliedTags([`${tagAccepted}`]);
+                await interaction.channel.setLocked(true);
+                return interaction.reply({ embeds: [completed] });
+            }
+
+            async function noPerms() {
+                return interaction.reply({ content: `You are not allowed to use this command.`, ephemeral: true });
+            }
+            async function notThread() {
+                return interaction.reply({ content: `You are not allowed to use this command in this channel.`, ephemeral: true });
+            }
+            async function isLocked() {
+                return interaction.reply({ content: `This suggestion is already completed.`, ephemeral: true });
+            }
+        }
+    }
+        
 }
---
+++ commands/utility/rules.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, EmbedBuilder, inlineCode } = require(`discord.js`);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName(`rules`).setDescription(`Refrences the rules.`)
+        .addSubcommand(subcommand => subcommand.setName(`full`).setDescription(`Sends the full rules message!`)
+            .addBooleanOption(option => option.setName(`ephemeral`).setDescription(`Hides the message from other members.`).setRequired(true)))
+        .addSubcommand(subcommand => subcommand.setName(`verify`).setDescription(`Verifies you.`)),
+
+    async execute(interaction) {
+        const sub = interaction.options.getSubcommand();
+
+        const member = interaction.member;
+
+        if (sub === `full`) {
+            fullMessage();
+        } else if (sub === `verify`) {
+            verifyUser();
+        }
+
+        async function fullMessage() {
+            const eph = interaction.options.getBoolean(`ephemeral`);
+
+            const fullRules = new EmbedBuilder()
+                .setTitle(`Server Rules`)
+                .addFields(
+                    { name: `1. General Discord-required rules (per the Community Guidelines)`, value: `a. Respect Each Other\nb. Be Honest\nc. Respect Discord\n[Learn more](https://discord.com/guidelines 'Discord Community Guidelines')` },
+                    { name: `2. Discord-wide rules (Terms of Service)`, value: `a. By accessing our services, you confirm that you’re at least 13 years old and meet the minimum age required by the laws in your country. ([List of minimum ages](https://support.discord.com/hc/en-us/articles/360040724612 'List of minimum ages'))\nb. Don’t use the services to do harm to yourself or others.\nc. Don’t use the services to do harm to Discord.\nd. Don’t use the services to do anything else that’s illegal.\n[Learn more](https://discord.com/terms 'Discord Terms of Service')` },
+                    { name: `3. Server-specific rules`, value: `a. Treat others with respect.\nb. When making jokes, tone tags (${inlineCode(`/j`)}, ${inlineCode(`/joke`)}, ${inlineCode(`/joking`)}) are required. (This makes moderation easier.)\nc. Keep all talk of sexual fantasies in our spicy server. (https://discord.gg/N3Zm48K4Z7)\nd. Put sensitive topics in spoilers. Sensitive topics can be (but are not limited to) guns, self harm, suicide, sexual assault. Spoilers can be created by adding ${inlineCode(`||`)} to the start and end of a message. (${inlineCode(`||this is a spoiler||`)})\ne. Please keep discussions of suicide and self harm to a minimum.\nf. This is an __english only__ server.` },
+                )
+                .setColor([111, 255, 164])
+
+            return interaction.reply({ embeds: [fullRules], ephemeral: eph })
+        }
+
+        async function verifyUser() {
+            return interaction.reply({ content: `Coming soon.`, ephemeral: true })
+        }
+    }
+}
